afab995eaf1402bbf18ed895a1490176fe3820f6
Marto001 <102255200+Marto001@users.noreply.github.com>
2022-04-06 15:47:04 -0300

    Add files via upload

---
+++ Vectores.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <Math.h>
+
+int int main(int argc, char const *argv[]) {
+  double x = atof(argv[3]) {;
+  double y = atof(argv[4]) {;
+  double distance = sqrt(x*x + y*y)
+  printf("la distancia entre (0,0) y (%.2f,%.2f) es %.2f", x, y, distance )
+  return 0;
+}
