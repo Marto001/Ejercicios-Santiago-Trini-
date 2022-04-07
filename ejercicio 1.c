d70e5953f514eddef1d7b010054bbdf6911c41e8
Marto001 <102255200+Marto001@users.noreply.github.com>
2022-04-07 12:31:29 -0300

    Add files via upload

---
+++ ejercicio 1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(int argc, char const *argv[]) {
+  double q= atoi(argv[1]);
+
+  printf("la suma del seno y el coseno es %f\n", cos(q)*cos(q) + sen(q)*sen(q) % );
+  return 0;
+}
